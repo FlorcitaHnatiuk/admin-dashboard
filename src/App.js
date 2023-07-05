e2f573e217b1d9dfdf314a76d9e1d76ab9e8aef6
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-07-05 15:14:39 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 import './App.css';
 
 const App = () => {
-    const activeMenu = false;
+    const activeMenu = true;
 
     return (
         <div>
@@ -34,36 +34,38 @@ const App = () => {
                     }>
                         <div className='fixed md:static bg-main-bg dark:big-main-dark-bg navbar w-full'>
                             <Navbar />
-                        </div>                   
+                        </div>
                     </div>
                     <div>
                         <Routes>
+                            {/* dashboard  */}
+                            <Route path="/" element={(<Ecommerce />)} />
+                            <Route path="/ecommerce" element={(<Ecommerce />)} />
 
-                            {/* Dashboard */}
-                            <Route path='/' element={<Ecommerce />}/>
-                            <Route path='/ecommerce' element={<Ecommerce />}/>
+                            {/* pages  */}
+                            <Route path="/orders" element={<Orders />} />
+                            <Route path="/employees" element={<Employees />} />
+                            <Route path="/customers" element={<Customers />} />
 
-                            {/* Pages */}
-                            <Route path='/orders' element={<Orders />} />
-                            <Route path='/employees' element={<Employees />} />
-                            <Route path='/customers' element={<Customers />} />
+                            {/* apps  */}
+                            <Route path="/kanban" element={<Kanban />} />
+                            <Route path="/editor" element={<Editor />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path="/color-picker" element={<ColorPicker />} />
 
-                            {/* Apps */}
-                            <Route path='/calendar' element={<Calendar />} />
-                            <Route path='/kanban' element={<Kanban />} />
-                            <Route path='/editor' element={<Editor />} />
-                            <Route path='/colorpicker' element={<ColorPicker />} />
+                            {/* charts  */}
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
 
-                            {/* Charts */}
-                            <Route path='/line' element={<Line />}/>
-                            <Route path='/area' element={<Area />}/>
-                            <Route path='/bar' element={<Bar/>}/>
-                            <Route path='/pie' element={<Pie/>}/>
-                            <Route path='/financial' element={<Financial />}/>
-                            <Route path='/colormapping' element={<ColorMapping />} />
-                            <Route path='/pyramid' element={<Pyramid />} />
                         </Routes>
                     </div>
+                    <Footer/>
                 </div>
             </BrowserRouter>
         </div>
